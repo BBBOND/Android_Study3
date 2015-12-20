754d4a2931a7b12c3bb69fd9ad1229e033a88cda
BBBOND <jwy8645@gmail.com>
2015-12-20 18:50:25 +0800

    学习传感器的设置和取值

--- sensor/src/main/java/com/kim/sensor/SensorActivity.java
+++ sensor/src/main/java/com/kim/sensor/SensorActivity.java
@@ -2,6 +2,8 @@ package com.kim.sensor;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +22,8 @@ public class SensorActivity extends AppCompatActivity {
     TextView sensorList;
     @Bind(R.id.show_sensor_list)
     Button showSensorList;
+    @Bind(R.id.sensor_value)
+    TextView sensorValue;
 
     private SensorManager sensorManager;
 
@@ -31,16 +35,48 @@ public class SensorActivity extends AppCompatActivity {
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
+        //显示所有传感器名及版本
         showSensorList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
                 String string = "";
                 for (Sensor sensor : sensors) {
-                    string += sensor.getName() + " " + sensor.getVersion() + "\n";
+                    string += sensor.getName() + "----->version" + sensor.getVersion() + "\n";
                 }
                 sensorList.setText(string);
             }
         });
+
+        //注册传感器及获取传感器精度和值
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensorManager.registerListener(new SensorEventListener() {
+
+            //传感器的值发生变化时调用
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                String sensorName = event.sensor.getName();
+                String sensorVendor = event.sensor.getVendor();
+                float resolution = event.sensor.getResolution();
+                float power = event.sensor.getPower();
+                float timestamp = event.timestamp;
+                float acc = event.accuracy;
+                float lux = event.values[0];
+                String value = "sensorName="+sensorName+"\nsensorVendor="+sensorVendor
+                        +"\nresolution="+resolution+"\npower="+power+"\ntimestamp="+timestamp
+                        +"\nacc=" + acc + "\nlux=" + lux;
+                sensorValue.setText(value);
+            }
+
+            //传感器的精度发生变化时调用
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        }, lightSensor, SensorManager.SENSOR_DELAY_GAME);
+        //SensorManager.SENSOR_DELAY_NORMAL = 200000ms
+        //SensorManager.SENSOR_DELAY_UI = 60000ms
+        //SensorManager.SENSOR_DELAY_GAME = 20000ms
+        //SensorManager.SENSOR_DELAY_FASTEST = 0ms
     }
 }
